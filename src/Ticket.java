c3bee26014c4232be7a42cbf2a4c5b3f29b81de7
s3861662 <67299159+s3861662@users.noreply.github.com>
2022-10-17 20:29:36 +1000

    Add files via upload
    Updated ticket to meet new requirements add LocalDate and overloaded constructor to take ticket creator.

--- src/Ticket.java
+++ src/Ticket.java
@@ -1,5 +1,7 @@
-
-
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.Deque;
 
 public class Ticket {
 
@@ -8,6 +10,10 @@ public class Ticket {
     private Status status;
     private Severity severity;
     private String description;
+    private LocalDate creationDate;
+    private LocalDate closedDate;
+    private User openBy;
+    private User closedBy;
 
     public Ticket(Severity severity) {
         //create an open ticket by default on creation.
@@ -24,6 +30,19 @@ public class Ticket {
         this.status = Status.OPEN;
         this.severity = severity;
         this.description = description;
+        //Create local date.
+        this.creationDate = LocalDate.now();
+    }
+
+    public Ticket(Severity severity, String description, User openBy) {
+        //create an open ticket by default on creation.
+        this.ticketNumber = "TN-" + autoInt++;
+        this.status = Status.OPEN;
+        this.severity = severity;
+        this.description = description;
+        this.openBy = openBy;
+        //Create local date.
+        this.creationDate = LocalDate.now();
     }
 
     public Status getStatus() {
@@ -52,6 +71,34 @@ public class Ticket {
 
     public  String getTicketNumber() {
         return this.ticketNumber;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public LocalDate getClosedDate() {
+        return closedDate;
+    }
+    //set the closed date.
+    public void setClosedDate() {
+        this.closedDate = LocalDate.now();
+    }
+
+    public User getOpenBy() {
+        return openBy;
+    }
+
+    public void setOpenBy(User openBy) {
+        this.openBy = openBy;
+    }
+
+    public User getClosedBy() {
+        return closedBy;
+    }
+
+    public void setClosedBy(User closedBy) {
+        this.closedBy = closedBy;
     }
 }
 
